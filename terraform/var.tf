e9ea35a740dd13bbf3d51aa2f34a93176a904db2
brico <bric0l@proton.me>
2025-04-12 18:06:47 +0200

    first

---
+++ terraform/var.tf
@@ -0,0 +1,47 @@
+variable "pve_insecure" {
+    type        = bool
+    description = "Enable insecure connexion"
+    default     = true
+}
+
+variable "pve_endpoint" {
+    type        = string
+    description = "API endpoint URL"
+}
+
+variable "pve_password" {
+    type        = string
+    description = "Password"
+}
+
+variable "pve_username" {
+    type        = string
+    description = "Username"
+}
+
+variable "pve_api_token" {
+    type        = string
+    description = "API token"
+}
+
+variable "pve_node" {
+    type        = string
+    description = "Node where install elements"
+    default     = ""
+}
+variable "debian_image_url" {
+    type        = string
+    description = "The URL for the latest Debian 12 Bookworm qcow2 image"
+    default     = ""
+}
+variable "debian_image_checksum_algorithm" {
+    type        = string
+    description = "Checksum algo used by image"
+    default      = "sha512"
+}
+
+variable "debian_image_checksum" {
+    type        = string
+    description = "SHA Digest of the image"
+    default     = ""
+}
