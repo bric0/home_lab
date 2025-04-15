2d0fe660c5385c33e203ec2c83c904eef30fe2b4
brico <bric0l@proton.me>
2025-04-15 21:45:38 +0200

    before changing to tailmate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,87 +1,95 @@
 resource "proxmox_virtual_environment_download_file" "ubuntu-24-04" {
-  content_type          = "iso"
-  datastore_id          = "local"
-  file_name             = "ubuntu-24.04.2-live-server-amd64.iso"
-  node_name             = var.pve_node
-  url                   = var.ubuntu_image_url
-  checksum              = var.ubuntu_image_checksum
-  checksum_algorithm    = var.ubuntu_image_checksum_algorithm
-  overwrite             = true
-  overwrite_unmanaged   = true
-#   verify = false
+  content_type        = "iso"
+  datastore_id        = "local"
+  file_name           = "ubuntu-24.04.2-live-server-amd64.iso"
+  node_name           = var.pve_node
+  url                 = var.ubuntu_image_url
+  checksum            = var.ubuntu_image_checksum
+  checksum_algorithm  = var.ubuntu_image_checksum_algorithm
+  overwrite           = true
+  overwrite_unmanaged = true
 }
 
 resource "proxmox_virtual_environment_file" "user_config" {
-  content_type  = "snippets"
-  datastore_id  = "local"
-  node_name     = var.pve_node
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.pve_node
 
   source_raw {
-    data        = file("cloud-init/user-config.yaml")
-    file_name   = "user-config.yaml"
+    data      = file("cloud-init/user-config.yaml")
+    file_name = "user-config.yaml"
   }
 }
 
 resource "proxmox_virtual_environment_file" "vendor_config" {
-  content_type  = "snippets"
-  datastore_id  = "local"
-  node_name     = var.pve_node
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.pve_node
 
   source_raw {
-    data        = file("cloud-init/vendor-config.yaml")
-    file_name   = "vendor-config.yaml"
+    data      = file("cloud-init/vendor-config.yaml")
+    file_name = "vendor-config.yaml"
   }
 }
 
+resource "proxmox_virtual_environment_file" "network_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.pve_node
+
+  source_raw {
+    data      = file("cloud-init/network-config.yaml")
+    file_name = "network-config.yaml"
+  }
+}
+
+
 resource "proxmox_virtual_environment_vm" "ubuntu-24-04" {
-    depends_on          = [ 
-        proxmox_virtual_environment_file.user_config,
-        proxmox_virtual_environment_file.vendor_config 
-    ]
-    name                = "home-controller-01"
-    description         = "Ubuntu 24.04  created with Terraform"
-    tags                = ["terraform", "ubuntu"]
-    node_name           = var.pve_node
+  depends_on = [
+    proxmox_virtual_environment_file.user_config,
+    proxmox_virtual_environment_file.vendor_config,
+    proxmox_virtual_environment_file.network_config
+  ]
+  name        = "home-controller-01"
+  description = "Ubuntu 24.04  created with Terraform"
+  tags        = ["terraform", "ubuntu"]
+  node_name   = var.pve_node
 
-    cpu {
-        cores   = 2
-    }
-    memory {
-        dedicated = 2048
-        floating  = 2048
-    }
+  cpu {
+    cores = 2
+  }
+  memory {
+    dedicated = 2048
+    floating  = 2048
+  }
 
-    disk {
-        datastore_id    = "local-lvm"
-        file_id         = proxmox_virtual_environment_download_file.ubuntu-24-04.id
-        interface       = "virtio0"
-        iothread        = true
-        discard         = "on"
-        ssd             = true
-    }
-    network_device {
-        bridge          = "vmbr0"
-        model           = "virtio"
-    }
-    operating_system {
-        type            = "l26"
-    }
-    agent {
-        enabled         = true
-    }
-    initialization {
-        # ou au lieu d'ip config utiliser network_data_file_id avec un fichier cloudinit
-        ip_config {
-            ipv4 {
-                address     = "dhcp"
-            }
-        }
-        # Link cloud-init yaml files
-        user_data_file_id   = proxmox_virtual_environment_file.user_config.id
-        vendor_data_file_id = proxmox_virtual_environment_file.vendor_config.id
-    }
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu-24-04.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    ssd          = true
+  }
+  network_device {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+  operating_system {
+    type = "l26"
+  }
+  agent {
+    enabled = true
+    timeout = "5m"
+  }
+
+  initialization {
+    network_data_file_id = proxmox_virtual_environment_file.network_config.id
+    user_data_file_id    = proxmox_virtual_environment_file.user_config.id
+    vendor_data_file_id  = proxmox_virtual_environment_file.vendor_config.id
+  }
 }
 
 output "ubuntu-24-04_test_ip_address" {
-    value = proxmox_virtual_environment_vm.ubuntu-24-04.ipv4_addresses
+  value = proxmox_virtual_environment_vm.ubuntu-24-04.ipv4_addresses
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_version = "> 1.6.0"
-    required_providers {
-        proxmox = {
-            source  = "bpg/proxmox"
-            version = "0.75.0"
-        }
+  required_version = "> 1.6.0"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.75.0"
     }
+  }
 }
 
 provider "proxmox" {
-    endpoint    = var.pve_endpoint
-    api_token   = var.pve_api_token
-    # username   = var.pve_username
-    # password   = var.pve_password
-    insecure    = var.pve_insecure
+  endpoint = var.pve_endpoint
+  # api_token   = var.pve_api_token
+  username = var.pve_username
+  password = var.pve_password
+  insecure = var.pve_insecure
 }
